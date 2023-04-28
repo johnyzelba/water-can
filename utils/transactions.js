665a66e6ecd37e6e3a2ca7fb9ae895aa9838dcb3
johnyzelba <johnyzelba@gmail.com>
2023-04-28 14:05:37 +0300

    Update transactions.js

--- utils/transactions.js
+++ utils/transactions.js
@@ -1,12 +1,12 @@
 const startTransaction = async (db) => {
     return new Promise(function (resolve, reject) {
-        db.begin((err, rows) => err ? reject(err) : resolve(rows));
+        db.run("BEGIN", (err, rows) => err ? reject(err) : resolve(rows));
     });
 };
 
 const endTransaction = async (db) => {
     return new Promise(function (resolve, reject) {
-        db.commit((err, rows) => err ? reject(err) : resolve(rows))
+        db.run('commit', (err, rows) => err ? reject(err) : resolve(rows))
     });
 };
 
