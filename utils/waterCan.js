ca4458bb27503476afd83fba441da3fe3005c836
johnyzelba <johnyzelba@gmail.com>
2023-07-26 12:15:58 +0300

    Update waterCan.js

--- utils/waterCan.js
+++ utils/waterCan.js
@@ -109,8 +109,8 @@ const addNutritions = async (potSize, nitrogen, phosphorus, potassium) => {
     stirrer.writeSync(0);
     const tempArr = new Array(40).fill('');
     for (let [index] of tempArr.entries()) {
-        await new Promise((res) => setTimeout(() => res(stirrer.writeSync(1)), 60));
-        await new Promise((res) => setTimeout(() => res(stirrer.writeSync(0)), 200 - (index * 2)));
+        await new Promise((res) => setTimeout(() => res(stirrer.writeSync(1)), 60 + index));
+        await new Promise((res) => setTimeout(() => res(stirrer.writeSync(0)), 400 - (index * 3)));
         console.log("----------", index);
     }
     
