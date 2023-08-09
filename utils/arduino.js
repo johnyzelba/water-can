7423a4832ed80d6089153f9e6e27ce11d0c6ae31
johnyzelba <johnyzelba@gmail.com>
2023-08-09 20:53:43 +0300

    1

---
+++ utils/arduino.js
@@ -0,0 +1,16 @@
+const serialport = require("serialport");
+
+const port = new serialport('/dev/ttyAMA0', { baudRate: 9600 });
+const Readline = serialport.parsers.Readline;
+const parser = new Readline();
+
+port.pipe(parser);
+
+const ping = async () => {
+    console.log(`GETTING LATEST PLANTS REPORTS FROM DB`);
+    parser.on('data', (data) => console.log("-----------data: ", data));
+    port.write('PING');
+    
+}
+
+module.exports = { ping };
