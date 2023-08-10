1192539db091ae79ddce97b07020d9a49c691a84
johnyzelba <johnyzelba@gmail.com>
2023-08-10 15:00:19 +0300

    Update arduino.js

--- utils/arduino.js
+++ utils/arduino.js
@@ -2,17 +2,18 @@ const { SerialPort } = require('serialport');
 
 const port = new SerialPort({
     path: '/dev/ttyUSB0',
-    baudRate: 19200,
-    dataBits: 8,
-    stopBits: 1,
-    parity: 'none',
+    baudRate: 9600,
 });
 
 const ping = async () => {
+    try {
     console.log(`PINGING ARDUINO`);
-    port.on('data', (data) => console.log("-----------data: ", data));
     port.write('PING');
-    
+    port.on('data', (data) => console.log("-----------data: ", data));
+    } catch(e) {
+        console.log(e);
+    }
+
 }
 
 module.exports = { ping };
